9f099ff35c5f245ec1770d2fad20553192bfc63c
Sumanth Neerumalla <sumanthneerumalla@users.noreply.github.com>
2018-02-08 14:06:24 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,11 @@
 #turn off that infernal bell sound
 echo "set bell-style none" >> ~/.inputrc
 
+#Make symbolic links
+ln -s /mnt/c/Users/Sumanth/Downloads Downloads
+ln -s /mnt/z/ z
+ln -s /mnt/c/Users/Sumanth/Desktop Desktop
+
 #addrepos
 sudo add-apt-repository ppa:pdfchain-team/ppa
 
@@ -24,3 +29,6 @@ echo "also add this to your .bashrc for easy uploads"
 echo ""
 echo " transfer() { if [ $# -eq 0 ]; then echo -e "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"; return 1; fi 
 tmpfile=$( mktemp -t transferXXX ); if tty -s; then basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g'); curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile; else curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile ; fi; cat $tmpfile; rm -f $tmpfile; }"
+
+echo""
+echo"Run the following to make a symbolic link \"ln -s /mnt/c/Users/Sumanth/Downloads Downloads\" "
